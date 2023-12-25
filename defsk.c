61164701e48a1b006123555c430c62398b828733
Richard Campbell <rlcamp@users.noreply.github.com>
2023-12-25 15:42:29 -0800

    initial commit

---
+++ defsk.c
@@ -0,0 +1,120 @@
+/* consume raw s16 pcm samples at 11025 sps, containing bell 203 modulation, and emit bytes
+ usage: ffmpeg -i /tmp/tmp.wav -f s16le - | ./defsk
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+
+static float cmagsquaredf(const float complex x) {
+    return crealf(x) * crealf(x) + cimagf(x) * cimagf(x);
+}
+
+static float complex renormalize(const float complex x) {
+    /* assuming x is already near unity, renormalize to unity w/o div or sqrt */
+    const float magsquared = cmagsquaredf(x);
+    return x * (3.0f - magsquared) * 0.5f;
+}
+
+int main(void) {
+    /* input arguments, all in cycles, samples, or symbols per second */
+    const float sample_rate = 11025, f_mark = 1270, f_space = 1070, baud = 300;
+
+    /* derived constants */
+    const size_t samples_per_bit = lrintf(sample_rate / baud);
+    const float complex advance_mark = cexpf(I * 2.0f * (float)M_PI * f_mark / sample_rate);
+    const float complex advance_space = cexpf(I * 2.0f * (float)M_PI * f_space / sample_rate);
+
+    /* 3 dB cutoff frequency for low pass filters centered on each frequency */
+    const float fc = 1.0f * baud;
+
+    /* compute alpha of an exponential filter centered at each of the two frequencies */
+    const float tmp = cosf(2.0f * (float)M_PI * fc / sample_rate);
+    const float alpha = tmp - 1.0f + sqrtf(tmp * tmp - 4.0f * tmp + 3.0f);
+
+    /* mag squared of response to space of the filter centered at mark, and vice versa */
+    const float opp_response = cmagsquaredf(alpha / (1.0f - (1.0f - alpha) * cexpf(I * 2.0f * (float)M_PI * (f_mark - f_space) / sample_rate)));
+
+    /* scaling factor for combining filter outputs to obtain a decision value */
+    const float normalize = 0.5f * (1.0f + opp_response) / (1.0f - opp_response);
+
+    /* carriers for the two tones, should always be (nearly) unit magnitude */
+    float complex carrier_mark = 1.0f, carrier_space = 1.0f;
+
+    /* response of the filters centered on each carrier */
+    float complex mark = 0.0f, space = 0.0f;
+
+    /* output value, with hysteresis */
+    int banged = 1;
+
+    /* the previous sample, used to determine if there has been a transition */
+    int banged_previous = 1;
+
+    /* stores the byte in progress */
+    unsigned char byte = 0;
+
+    /* more state variables */
+    size_t samples_since_last_transition = SIZE_MAX;
+    size_t samples_since_last_bit = SIZE_MAX;
+    size_t ibit = 9;
+
+    /* loop over raw pcm samples on stdin */
+    for (int16_t sample; fread(&sample, sizeof(int16_t), 1, stdin) > 0; ) {
+        /* maintain filters around each of the two possible carriers */
+        mark = mark + alpha * (sample * carrier_mark - mark);
+        space = space + alpha * (sample * carrier_space - space);
+
+        /* power in the separate mark and space filters */
+        const float mm = cmagsquaredf(mark), ss = cmagsquaredf(space);
+
+        /* a number between 0 and 1, with a bunch of noise and ripple */
+        const float normalized = 0.5f + normalize * (mm - ss) / (mm + ss);
+
+        /* either 0 or 1, with some hysteresis for debouncing */
+        banged = banged ? (normalized < 0.25f ? 0 : 1) : (normalized < 0.75f ? 0 : 1);
+
+        /* maintain carriers */
+        carrier_mark = renormalize(carrier_mark * advance_mark);
+        carrier_space = renormalize(carrier_space * advance_space);
+
+        /* what follows is some very quick and dirty code from 2014 that takes oversampled,
+         thresholded input and converts it to serial bytes, assuming 8N1 encoding, emitting
+         framing errors to stderr. this was the first, most hackish thing that i came up
+         with, and further attempts at cleverness at the time failed to come up with anything
+         that worked better. nevertheless, significant improvements are likely possible */
+        samples_since_last_transition++;
+        samples_since_last_bit++;
+
+        if (banged_previous != banged)
+            samples_since_last_transition = 0;
+
+        if (9 == ibit &&
+            0 == banged &&
+            samples_since_last_transition == samples_per_bit / 2 &&
+            samples_since_last_bit >= 3 * samples_per_bit / 2) {
+            /* detected a down transition at the start of a byte */
+            ibit = 0;
+            samples_since_last_bit = 0;
+        }
+        else if (ibit < 9 && samples_since_last_bit == samples_per_bit) {
+            /* if this was the end-of-byte symbol... */
+            if (8 == ibit) {
+                /* if the end-of-byte symbol was correct, emit complete byte */
+                if (1 == banged) putchar(byte);
+
+                /* otherwise print a warning to stderr */
+                else fprintf(stderr, "warning: %s: discarding possible %#x\n",
+                    __func__, byte);
+            } else {
+                /* set or clear this bit in the byte in progress */
+                byte = (byte & ~(1 << ibit)) | (banged ? (1 << ibit) : 0);
+
+                samples_since_last_bit = 0;
+            }
+
+            ibit++;
+        }
+
+        banged_previous = banged;
+    }
+}
---
+++ fsk.c
@@ -0,0 +1,46 @@
+/* usage: printf 'hello' | ./fsk | ffmpeg -y -f s16le -ar 11025 -i - /tmp/tmp.wav */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+
+static float cmagsquaredf(const float complex x) {
+    return crealf(x) * crealf(x) + cimagf(x) * cimagf(x);
+}
+
+int main(void) {
+    /* input arguments, all in cycles, samples, or symbols per second */
+    const float sample_rate = 11025, f_mark = 1270, f_space = 1070, baud = 300;
+
+    /* derived constants */
+    const float samples_per_bit = sample_rate / baud;
+    const float complex advance_mark = cexpf(I * 2.0f * (float)M_PI * f_mark / sample_rate);
+    const float complex advance_space = cexpf(I * 2.0f * (float)M_PI * f_space / sample_rate);
+
+    /* initial state */
+    float complex carrier = 1.0f;
+    float samples_since_bit_start = -1.0f * samples_per_bit;
+
+    /* emit one bit period of mark tone so that decoder sees mark-to-space transition */
+    for (; samples_since_bit_start < 0.0f; samples_since_bit_start++) {
+        fwrite(&(int16_t) { lrintf(cimagf(carrier) * 32767.0f) }, sizeof(int16_t), 1, stdout);
+        carrier *= advance_mark;
+    }
+
+    /* for each byte on stdin... */
+    for (int byte; (byte = fgetc(stdin)) != EOF; )
+        /* for each bit (including start and stop bits)... */
+        for (size_t ibit = 0; ibit < 10; ibit++, samples_since_bit_start -= samples_per_bit) {
+            const int bit = ibit < 1 ? 0 : ibit < 9 ? byte & (1 << (ibit - 1)) : 1;
+
+            /* for each sample, accounting for noninteger sample rate over baud... */
+            for (; samples_since_bit_start < samples_per_bit; samples_since_bit_start++) {
+                fwrite(&(int16_t) { lrintf(cimagf(carrier) * 32767.0f) }, sizeof(int16_t), 1, stdout);
+                carrier *= bit ? advance_mark : advance_space;
+            }
+
+            /* assuming carrier is still near unity, renormalize to unity w/o div or sqrt */
+            const float magsquared = cmagsquaredf(carrier);
+            carrier *= (3.0f - magsquared) * 0.5f;
+        }
+}
